dd30998005d696bec32c7253b999f41d7ac32f47
张巍 <zhangwei@3laoshi.com>
2020-04-26 10:20:25 +0800

    feat: add strconv

--- env.go
+++ env.go
@@ -1,6 +1,8 @@
 package goenv
 
 import (
+	"strconv"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -32,6 +34,34 @@ func loadFile(file string) {
 	}
 }
 
-func Getenv(s string) string {
-	return EnvMap[s]
+func Getenv(value string) string {
+	return EnvMap[value]
+}
+
+func String(value string) string {
+	return Getenv(value)
+}
+
+func Int(value string) int {
+	v := Getenv(value)
+	if s, err := strconv.Atoi(v); err == nil {
+		return s
+	}
+	return 0
+}
+
+func Boolean(value string) bool {
+	v := Getenv(value)
+	if s, err := strconv.ParseBool(v); err == nil {
+		return s
+	}
+	return false
+}
+
+func Float(value string) float64 {
+	v := Getenv(value)
+	if s, err := strconv.ParseFloat(v, 64); err == nil {
+		return s
+	}
+	return 0
 }
